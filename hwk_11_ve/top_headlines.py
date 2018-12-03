72a8741c53332f57d20ce5652dcfb6edfcd26929
Ryo Togami <ryo6122@0587357540.wireless.umich.net>
2018-12-02 22:18:02 -0500

    finished doing HW12

---
+++ hwk_11_ve/top_headlines.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import requests
+import json
+from apikey import params
+
+
+app = Flask(__name__) # app is named from the name of file
+
+@app.route('/')
+def index():
+    return '<h1>Welcome!</h1>'
+
+
+
+#nm = input("what is the name ")
+@app.route('/user/<nm>' ) #decorating function
+def user_name(nm):
+    baseurl ='https://api.nytimes.com/svc/topstories/v2/'
+    url = baseurl + 'technology' +'.json'
+    data =requests.get(url, params).json()
+    count = 0
+    data_list = []
+
+    for i in data["results"]:
+        count += 1
+        data_list.append(str(count) + ". " + str(i["title"]) + " (" + str(i["url"]) + ") ")
+        #print(count)
+        if (count == 5):
+            break
+    return render_template('user.html', name=nm, my_list=data_list)
+
+
+if __name__ == '__main__':
+    print('starting Flask app', app.name)
+    app.run(debug=True)
